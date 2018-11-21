b24e91a66924f84fc7f62642297d285e8ac468b7
Graeme Robinson <1robig68@solent.ac.uk>
2018-11-21 08:34:33 +0000

    improved js structure
    added else to if statement, tidied js structure

--- js/clock.js
+++ js/clock.js
@@ -1,28 +1,25 @@
- function showTime() {
-           var date = new Date();
-           var h = date.getHours(); // 0 - 23
-           var m = date.getMinutes(); // 0 - 59
-           var s = date.getSeconds(); // 0 - 59
-           var session = "AM";
+function showTime() {
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  
+    if(h >= 12){
+     var session = "PM";
+    } else{
+      var session = "AM";
+    }
 
-           if(h == 0){
-            h = 12;
-           }
-
-           if(h >= 12){
-            h = h - 12;
-            session = "PM";
-           }
-
-           h = (h < 10) ? "0" + h : h;
-           m = (m < 10) ? "0" + m : m;
-           s = (s < 10) ? "0" + s : s;
+      h = (h < 10) ? "0" + h : h;
+      m = (m < 10) ? "0" + m : m;
+      s = (s < 10) ? "0" + s : s;
         
-           var time = h + ":" + m + /*":" + s + */" " + session;
-           document.getElementById("myClock").innerText = time; //for firefox users
-           document.getElementById("myClock").textContent = time; // for I.E users
+        var time = h + ":" + m + ":" + s + " " + session;
+        
+        document.getElementById("myClock").innerText = time; //for firefox users
+        document.getElementById("myClock").textContent = time; // for I.E users
 
-           setTimeout(showTime, 1000);
-        }
+        setTimeout(showTime, 1000);
+}
 
 showTime();
