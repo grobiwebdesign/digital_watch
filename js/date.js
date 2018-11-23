85dd34a88005e2a87f17d772bdc546934db196a7
Graeme Robinson <1robig68@solent.ac.uk>
2018-11-23 17:53:20 +0000

    sass
    updated project to use SCSS, and included a JS file with scripts for the current date

--- js/date.js
+++ js/date.js
@@ -1,23 +1,67 @@
 function showDate() {
 		var date = new Date();
-		var d = date.getDay(); // dd
+		var d = date.getDate(); // dd
 		var m = date.getMonth()+1; // mmm
 		var y = date.getFullYear() // yyyy
 
-		if(d < 10){
+		if(d < 10){ //show single digit if less than 10
 			d = "0" + d;
 		}
 
-		if (m  < 10) {
+		if (m  < 10) { // show single digit if less than 10
 			m = "0" +  m;
 		}
 
-		if (m == 11){
-			m = "Nov";
+		if (m == 1){
+		m = "Jan";
 		}
 
-		var today = d + ':' + m + ':' + y;
-		document.getElementById('date').innerHTML = today;
+		if (m == 2){
+		m = "Feb";
+		}
+
+		if (m == 3){
+		m = "Mar";
+		}
+
+		if (m == 4){
+		m = "Apr";
+		}
+
+		if (m == 5){
+		m = "May";
+		}
+
+		if (m == 6){
+		m = "Jun";
+		}
+
+		if (m == 7){
+		m = "Jul";
+		}
+
+		if (m == 8){
+		m = "Aug";
+		}
+
+		if (m == 9){
+		m = "Sept";
+		}
+
+		if (m == 10){
+		m = "Oct";
+		}
+
+		if (m == 11){
+		m = "Nov";
+		}
+
+		if (m == 12){
+		m = "Dec";
+		}
+
+		var today = d + ' ' + m + ' ' + y;
+		document.getElementById('todaysDate').innerHTML = today;
 }
 
 showDate();
